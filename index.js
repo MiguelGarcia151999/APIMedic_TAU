44fdf6c31e8daab4db55911af22eea543de07d90
MiguelGarcia151999 <ubuntu@ip-172-31-83-36.ec2.internal>
2023-05-08 20:05:06 +0000

    cambios

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8080
 
 //* Invocando las rutas
 app.use('/api',require('./routes'))
