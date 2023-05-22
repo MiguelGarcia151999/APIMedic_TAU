13b3e1f567f3f14b75cbd5baf84390f50b68edb1
MiguelGarcia151999 <ubuntu@ip-172-31-83-36.ec2.internal>
2023-05-22 19:38:38 +0000

    cambios

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 require('dotenv').config()
 const express = require("express")
 const cors = require("cors")
-const dbConnect = require('./config/mongo')
+const Connection = require('./config/mongo');
+const morgan = require("morgan");
+const helmet = require("helmet");
 const app = express()
+
+app.disable("x-powered-by");
+app.use(morgan("dev"));
+app.use(helmet());
 
 app.use(cors())
 app.use(express.json())
@@ -10,10 +16,8 @@ app.use(express.json())
 const port = process.env.PORT || 8080
 
 //* Invocando las rutas
-app.use('/api',require('./routes'))
+app.use('/api',require('./server'));
 
 app.listen(port, () => {
     console.log(`Servidor corriendo en el puerto: ${port}`)
 })
-
-dbConnect()
